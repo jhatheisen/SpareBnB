f44e7c54b7648aae96c2e70f42a83c52d40a807f
Jhass Theisen <jhasstheisen1@gmail.com>
2022-12-14 22:16:09 -0800

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -12,12 +12,17 @@ const isProduction = environment === 'production';
 
 const app = express();
 
+const routes = require('./routes');
+
+const { ValidationError } = require('sequelize');
+
 // for logging information about requests and responses:
 app.use(morgan('dev'));
 
 // cookie-parser middleware for parsing cookies and the express.json middleware for parsing JSON bodies of requests with Content-Type of "application/json"
 app.use(cookieParser());
 app.use(express.json());
+
 
 // Security Middleware
 if (!isProduction) {
@@ -30,24 +35,51 @@ app.use(
   helmet.crossOriginResourcePolicy({
     policy: "cross-origin"
   })
-);
+  );
 
-// Set the _csrf token and create req.csrfToken method
-app.use(
-  csurf({
-    cookie: {
-      secure: isProduction,
-      sameSite: isProduction && "Lax",
-      httpOnly: true
-    }
-  })
-);
+  // Set the _csrf token and create req.csrfToken method
+  app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+    );
 
-// backend/app.js
-const routes = require('./routes');
+    // Connect all the routes
+    app.use(routes);
 
-// ...
+    // Catch unhandled requests and forward to error handler.
+    app.use((_req, _res, next) => {
+      const err = new Error("The requested resource couldn't be found.");
+      err.title = "Resource Not Found";
+      err.errors = ["The requested resource couldn't be found."];
+      err.status = 404;
+      next(err);
+    });
 
-app.use(routes); // Connect all the routes
+    // Process sequelize errors
+    app.use((err, _req, _res, next) => {
+      // check if error is a Sequelize error:
+      if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 
 module.exports = app;
