9bc0da1f16182ab2e8bba0aea5c232129383f34b
Jhass Theisen <jhasstheisen1@gmail.com>
2022-12-15 22:30:28 -0800

    Add User login backend endpoint

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -19,18 +19,16 @@ module.exports = (sequelize, DataTypes) => {
       return User.scope('currentUser').findByPk(id);
     }
 
-    static async login( {credential, password} ) {
+    static async login({ credential, password }) {
       const { Op } = require('sequelize');
-
       const user = await User.scope('loginUser').findOne({
         where: {
           [Op.or]: {
             username: credential,
-            email: credential,
+            email: credential
           }
         }
       });
-
       if (user && user.validatePassword(password)) {
         return await User.scope('currentUser').findByPk(user.id);
       }
