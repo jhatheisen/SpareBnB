a4f2c0bfd560f9055c704ea16ed2d8b98606899d
Jhass Theisen <jhasstheisen1@gmail.com>
2023-01-13 15:08:29 -0800

    Finished phase 3, logout, nav, and profile dd

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,36 +1,30 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { useSelector, useDispatch} from "react-redux";
+import { useSelector} from "react-redux";
 import ProfileButton from "./ProfileButton";
-import * as sessionActions from '../../store/session';
 import './Navigation.css';
 
 function Navigation({isLoaded}) {
   const sessionUser = useSelector(state => state.session.user);
-  const dispatch = useDispatch();
-
-  const logout = (e) => {
-    e.preventDefault();
-    dispatch(sessionActions.logout());
-  }
 
   let sessionLinks;
   if (sessionUser) {
     sessionLinks = (
       <li>
         <ProfileButton user={sessionUser} />
-        <button onClick={logout}>Log Out</button>
       </li>
     );
   } else {
     sessionLinks =(
     <>
-      <li>
-        <NavLink to='/login'>Log In</NavLink>
-      </li>
-      <li>
-        <NavLink to='/signup'>Sign Up</NavLink>
-      </li>
+      <div>
+        <li>
+          <NavLink to='/login'>Log In</NavLink>
+        </li>
+        <li>
+          <NavLink to='/signup'>Sign Up</NavLink>
+        </li>
+      </div>
     </>
     );
   }
