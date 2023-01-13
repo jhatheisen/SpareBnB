4c0d23a0bd5839251e3b2e74c8d1528ce122d73e
Jhass Theisen <jhasstheisen1@gmail.com>
2023-01-13 09:03:44 -0800

    Frontend Setup Complete

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,19 @@ import React from 'react';
 import './index.css';
 
 import ReactDOM from 'react-dom';
-import { Provider } from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 import configureStore from './store';
+import { csrfFetch, restoreCSRF } from './store/csrf';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
