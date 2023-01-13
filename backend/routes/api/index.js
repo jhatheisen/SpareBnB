04edb6250d7c7c8ee1093af5d9cd36893439eb3f
Jhass Theisen <jhasstheisen1@gmail.com>
2023-01-13 09:05:17 -0800

    Finished frontend assisting setup

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -9,6 +9,10 @@ const imagesRouter = require('./images.js');
 const spotImagesRouter = require('./spot-images.js');
 const { restoreUser } = require("../../utils/auth.js");
 
+// router.post('/test', function(req, res) {
+//   res.json({ requestBody: req.body });
+// });
+
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
   // If current user session is not valid, set req.user to null
